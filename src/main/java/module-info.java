ceffa328f7d84f0c875e8e9804213bae961b96aa
dbc2201 <divyanshb22@gmail.com>
2022-02-18 21:51:39 +0530

    added library for log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.dbc.github.pleasely {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.apache.logging.log4j;
 
     opens io.dbc.github.pleasely to javafx.fxml;
     exports io.dbc.github.pleasely;
